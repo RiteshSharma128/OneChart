caef0868525a82296f0ace0007742a7065ac11d1
Ritesh Sharma <rs3907367@gmail.com>
2025-09-27 13:16:08 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ let app=express()
 app.use(express.json())
 app.use(cookieParser()) // cookies ko pass karane ke liye cookies ka use karate hai
 app.use(cors({
-origin:["https://onechart-ritesh-sharma.onrender.com","http://localhost:5174"],
+origin:["https://onechart-ritesh-sharma.onrender.com","https://onechart-riteshadmin.onrender.com"],
 credentials:true
 }))
 
